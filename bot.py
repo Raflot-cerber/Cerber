09ccc20bbfd71f302f40f74470ae734bc8d8dcab
Raflot-cerber <swsw49350@gmail.com>
2025-08-08 13:30:49 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot connecté en tant que {bot.user}")
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+
+bot.run("TON_TOKEN")
