3702b36cc4a7e6869fb411e2b94c2491d83ab6e5
Raflot-cerber <swsw49350@gmail.com>
2025-08-08 15:22:50 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,14 +4,12 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-# Charger les variables depuis le fichier .env
 load_dotenv()
 
-# Récupérer le token depuis la variable d'environnement
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # Important !
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
